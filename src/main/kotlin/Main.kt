e8a2a9f596e98ebf68cf25e9b249c42e758fdee5
Artana-A <apple.aisyahputriarta@gmail.com>
2026-07-15 10:24:15 +0700

    Adding choose character

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.example
 
+import org.example.Character
+
 fun main() {
     println("""
         ====================================================
@@ -26,5 +28,27 @@ fun main() {
         return
     }
 
+    choice  = 0
+    while(choice != 1 && choice != 2){
+        println("\n\nSelect your character!")
+        println("1. Use pre-made character")
+        println("2. Customize your own character")
+        print("Your choice : ")
+        choice = readln().toIntOrNull()?:0
+        if (choice != 1 && choice != 2) {
+            print("\nChoice invalid! Enter number 1 or 2\n")
+        }
+
+    }
+
+    val player = if (choice == 1){
+        chooseCharacter(listCharacter)
+    } else {
+        makeNewCharacter()
+    }
+
+    printPlayer(player)
 
 }
+
+
--- src/main/kotlin/Main_Functions.kt
+++ src/main/kotlin/Main_Functions.kt
@@ -1,27 +1,44 @@
 package org.example
 
-fun makeNewCharacter(){
-    print("Insert new character name: ")
-    var newCharacterName = readln()
+fun makeNewCharacter(): Character {
+    print("\nInsert new character name: ")
+    var newCharacterName = readln().ifBlank { "Bob" }
 
-    var newCharacter = Character(
+    print("Insert new character time (minutes): ")
+    var newCharacterTime = readln().toIntOrNull()
+
+    if (newCharacterTime == null) {
+        newCharacterTime = 50
+    }
+
+    print("Insert new character money: ")
+    var newCharacterMoney = readln().toFloatOrNull()
+
+    if (newCharacterMoney == null) {
+        newCharacterMoney = 30000f
+    }
+
+    return Character(
         name = newCharacterName,
-        50,
-        25000f)
+        availableTime = newCharacterTime,
+        availableMoney = newCharacterMoney
+    )
+
+
 }
 
 fun chooseCharacter(availableCharacter: List<Character>):Character {
     var choice = 0
     while (choice !in 1..listCharacter.size) {
-        println("=== CHOOSE YOUR CHARACTER ===")
-
+        println("\nChoose your character!\n")
         for (i in availableCharacter.indices){
             println("""
                 Character #${i+1} 
-                ${availableCharacter[i].name} 
-                (Waktu: ${availableCharacter[i].availableTime}
-                Uang: Rp${availableCharacter[i].availableMoney})
+                Name    : ${availableCharacter[i].name} 
+                Time    : ${availableCharacter[i].availableTime}
+                Money   : Rp${availableCharacter[i].availableMoney}
                 """.trimIndent())
+            print("\n")
         }
 
         print("Your choice: ")
@@ -31,4 +48,16 @@ fun chooseCharacter(availableCharacter: List<Character>):Character {
     var player = availableCharacter[choice-1]
 
     return player
+}
+
+fun printPlayer(chosenPlayer: Character){
+    print("\n")
+    println("""
+                Chosen Character 
+                Name    : ${chosenPlayer.name} 
+                Time    : ${chosenPlayer.availableTime}
+                Money   : Rp${chosenPlayer.availableMoney}
+                """.trimIndent()
+    )
+
 }
