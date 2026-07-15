1bae22c48741ff410fea2192cbd2acc41f8f2d0c
Artana-A <apple.aisyahputriarta@gmail.com>
2026-07-15 18:54:48 +0700

    Add helper function quitOrNext

---
+++ src/main/kotlin/Helper_Functions.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun quitOrNext(){
+    println("[Press enter to continue, enter q to quit]")
+    var choice = readln().trim().lowercase()
+    if (choice == "q" || choice == "quit" ) {
+        print("\nChoice invalid! Enter or choose q\n")
+       return
+    }
+}
